ead56d32eba83a9dddd6778e0a49f095b1dca032
Alejandro Maguey <crookedx01@gmail.com>
2014-10-15 13:02:55 -0500

    A good base for the project

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 200) {
+        $('.main-header').addClass('sticky');
+    } else {
+        $('.main-header').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main-header').hasClass('open-nav')) {
+        $('.main-header').removeClass('open-nav');
+    } else {
+        $('.main-header').addClass('open-nav');
+    }
+});
+
+$('.main-header li a').click(function() {
+    if ($('.main-header').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main-header').removeClass('open-nav');
+    }
+});
+
+
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
